3eadb2999e6569624272921d13a948c5b131787e
David Uzumaki <56260075+duzumaki@users.noreply.github.com>
2024-10-14 19:21:37 +0100

    Add OAUTH_DEVICE_VERIFICATION_URI setting

--- oauth2_provider/views/device.py
+++ oauth2_provider/views/device.py
@@ -6,11 +6,14 @@ from django.views.decorators.csrf import csrf_exempt
 from oauth2_provider.compat import login_not_required
 from django.utils.decorators import method_decorator
 from django import http
+from oauthlib.oauth2.rfc8628.pre_configured import DeviceApplicationServer
 
 
 @method_decorator(csrf_exempt, name="dispatch")
 @method_decorator(login_not_required, name="dispatch")
 class DeviceAuthorizationView(OAuthLibMixin, View):
+    server_class = DeviceApplicationServer
+
     def post(self, request, *args, **kwargs):
         headers, body, status = self.create_device_authorization_response(request)
 
