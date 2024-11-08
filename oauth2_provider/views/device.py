c52b2d1184682dd5d57cf604016b06194f12483c
David Uzumaki <56260075+duzumaki@users.noreply.github.com>
2024-11-08 15:21:24 +0000

    create device obj

--- oauth2_provider/views/device.py
+++ oauth2_provider/views/device.py
@@ -8,6 +8,8 @@ from django.utils.decorators import method_decorator
 from django import http
 from oauthlib.oauth2.rfc8628.endpoints import DeviceApplicationServer
 from oauth2_provider.models import Device, create_device
+from oauth2_provider.models import DeviceRequest, DeviceCodeResponse
+from urllib.parse import parse_qs
 
 
 @method_decorator(csrf_exempt, name="dispatch")
@@ -17,5 +19,9 @@ class DeviceAuthorizationView(OAuthLibMixin, View):
 
     def post(self, request, *args, **kwargs):
         headers, data, status = self.create_device_authorization_response(request)
-        create_device()
+
+        parsed_body: dict[str, list[str]] = parse_qs(request.body.decode())
+        device_request = DeviceRequest(**{k: v[0] for k, v in parsed_body.items()})
+        device_response = DeviceCodeResponse(**data)
+        create_device(device_request, device_response)
         return http.JsonResponse(data=data, status=status, headers=headers)
