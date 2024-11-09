2cb869ab94b45881e7e0b699f3f2fa71d150ab7c
David Uzumaki <56260075+duzumaki@users.noreply.github.com>
2024-11-09 18:32:31 +0000

    device update

--- oauth2_provider/views/device.py
+++ oauth2_provider/views/device.py
@@ -7,7 +7,7 @@ from oauth2_provider.compat import login_not_required
 from django.utils.decorators import method_decorator
 from django import http
 from oauthlib.oauth2.rfc8628.endpoints import DeviceApplicationServer
-from oauth2_provider.models import Device, create_device
+from oauth2_provider.models import Device, get_or_create_device
 from oauth2_provider.models import DeviceRequest, DeviceCodeResponse
 from urllib.parse import parse_qs
 
@@ -23,5 +23,7 @@ class DeviceAuthorizationView(OAuthLibMixin, View):
         parsed_body: dict[str, list[str]] = parse_qs(request.body.decode())
         device_request = DeviceRequest(**{k: v[0] for k, v in parsed_body.items()})
         device_response = DeviceCodeResponse(**data)
-        create_device(device_request, device_response)
+        get_or_create_device(device_request, device_response)
+
         return http.JsonResponse(data=data, status=status, headers=headers)
+
